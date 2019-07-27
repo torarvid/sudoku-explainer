f6cb965404b774faeffc91c4bff90a3f9003f644
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2019-07-28 00:21:30 +0200

    Move 'fe' to 'docs'
    Easy gh-pages perhaps?

--- be/server.js
+++ be/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static('fe'))
+app.use(express.static('docs'))
 
 const listener = app.listen(8008, () => {
     console.log(`Listening on ${listener.address().port}`)
